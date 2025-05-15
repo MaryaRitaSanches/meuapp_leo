516c338f0791e8d7e2eda07cd00994e76c193433
MaryaRitaSanches <marya.souza4@etec.sp.gov.br>
2025-05-15 20:32:38 -0300

    Segundo commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,19 +20,19 @@ export default function Layout() {
     <Tabs.Screen name= 'settings' 
     options= {{
       title : 'Configurações',
-      tabBarIcon: ({color, size}) => <Ionicons name = 'home-outline' size = {size} color = {color}/>
+      tabBarIcon: ({color, size}) => <Ionicons name = 'settings-outline' size = {size} color = {color}/>
     }}
     />
-    <Tabs.Screen name= 'produts' 
+    <Tabs.Screen name= 'products' 
     options= {{
       title : 'Produtos',
-      tabBarIcon: ({color, size}) => <Ionicons name = 'home-outline' size = {size} color = {color}/>
+      tabBarIcon: ({color, size}) => <Ionicons name = 'products-outline' size = {size} color = {color}/>
     }}
     />
      <Tabs.Screen name= 'sales' 
     options= {{
       title : 'Vendas',
-      tabBarIcon: ({color, size}) => <Ionicons name = 'home-outline' size = {size} color = {color}/>
+      tabBarIcon: ({color, size}) => <Ionicons name = 'sales-outline' size = {size} color = {color}/>
     }}
     />
     
